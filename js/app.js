728acd3ad06c3c9b01fd1d36c455ed4590cee737
Wayne Graham <wgraham@clir.org>
2015-12-10 08:13:57 -0500

    JS Dependency Loading Update
    Update templates to be able to require JS dependencies on a view
    Only load the RSS parser on the first page
    
    #1

--- js/app.js
+++ js/app.js
@@ -14,16 +14,6 @@ $(document).ready(function(){
       mobile: true
     }
   );
+
   wow.init();
-
-  $('#blog').FeedEk({
-    FeedUrl: 'http://diglib.org/feed',
-    MaxCount: 5,
-    ShowDesc: true,
-    ShowPubDate: true,
-    DescCharacterLimit: 100,
-    TitleLinkTarget: '_blank'
-  });
-
-
 });
