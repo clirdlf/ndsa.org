f2fae956e43568ce50893b56ecf65891f17ea8e1
Wayne Graham <wgraham@clir.org>
2016-01-03 10:26:33 -0500

    Combine RSS feed
    Uses chimpFeedr to combine RSS feed from LoC and diglib.org
    
    Anything tagged 'ndsa' on diglib.org will be included.

--- js/feed.js
+++ js/feed.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $('#blog').FeedEk({
-    FeedUrl: 'http://blogs.loc.gov/digitalpreservation/category/ndsa-2/feed/',
+    // FeedUrl: 'http://blogs.loc.gov/digitalpreservation/category/ndsa-2/feed/',
+    FeedUrl: 'http://mix.chimpfeedr.com/7f8d0-NDSA', // mix the feed for LoC & diglib
     MaxCount: 5,
     ShowDesc: true,
     ShowPubDate: true,
