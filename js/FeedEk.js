34107987bfd211d3cf1d89e8da4b39818194bc27
Wayne Graham <wgraham@clir.org>
2016-01-03 10:14:01 -0500

    Update RSS Styless
    - Better loader image that gets rid of the jaggies
    - RSS div uses max-width style (same as twitter)
    - Adds same border radius as Twitter
    
    closes #39

--- js/FeedEk.js
+++ js/FeedEk.js
@@ -19,7 +19,7 @@
         var id = $(this).attr("id"), i, s = "", dt;
         $("#" + id).empty();
         if (def.FeedUrl == undefined) return;
-        $("#" + id).append('<img src="loader.gif" />');
+        $("#" + id).append('<img class="img-responsive center-block" width="40px" src="images/loader.gif" />');
 
         var YQLstr = 'SELECT channel.item FROM feednormalizer WHERE output="rss_2.0" AND url ="' + def.FeedUrl + '" LIMIT ' + def.MaxCount;
 
