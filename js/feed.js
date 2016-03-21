bb89aa700c27854e03c712751ce53dc8d350ef5d
Bethany Nowviskie <nowviskie@gmail.com>
2016-03-21 12:41:24 -0400

    updating blog feed

--- js/feed.js
+++ js/feed.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $('#blog').FeedEk({
-    // FeedUrl: 'http://blogs.loc.gov/digitalpreservation/category/ndsa-2/feed/',
+    // FeedUrl: 'https://www.diglib.org/topics/community/dlfcollaborations/ndsa/feed/',
     FeedUrl: 'http://mix.chimpfeedr.com/7f8d0-NDSA', // mix the feed for LoC & diglib
     MaxCount: 5,
     ShowDesc: true,
