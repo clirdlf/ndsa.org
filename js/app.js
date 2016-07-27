59371c2ddd6549f53a5d16716df32a83562ae648
Wayne Graham <wgraham@clir.org>
2016-07-27 08:41:40 -0400

    Add email protection

--- js/app.js
+++ js/app.js
@@ -25,4 +25,17 @@ $(document).ready(function(){
     window.location = $(this).find('a').attr('href');
     return false;
   });
+
+  /* Simple spam protection for email addresses using jQuery.
+   * Well, the protection isn’t jQuery-based, but you get the idea.
+   * This snippet allows you to slightly ‘obfuscate’ email addresses to make it harder for spambots to harvest them, while still offering a readable address to your visitors.
+   * E.g.
+   * <a href="mailto:foo(at)example(dot)com">foo at example dot com</a>
+   * →
+   * <a href="mailto:foo@example.com">foo@example.com</a>
+   */
+  $('a[href^="mailto:"]').each(function() {
+    this.href = this.href.replace('(at)', '@').replace(/\(dot\)/g, '.');
+    this.innerHTML = this.href.replace('mailto:', '');
+  });
 });
